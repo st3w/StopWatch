de62cdf175e4db9bd1edd662e4ba61b7c7318139
stewart <stewart@zyzz.(none)>
2013-05-29 23:18:08 -0400

    First commit with working stopwatch

---
+++ StopWatch.h
@@ -0,0 +1,36 @@
+#ifndef STOP_H
+#define STOP_H
+
+#include <chrono>
+using namespace std::chrono;
+
+class StopWatch
+{
+public:
+    StopWatch();
+    void tick(); // tick at the end of main game loop
+    float getTimeElapsed() const;
+
+private:
+    high_resolution_clock::time_point newTime, oldTime;
+};
+
+StopWatch::StopWatch()
+{
+    oldTime = high_resolution_clock::now();
+    newTime = oldTime;
+}
+
+void StopWatch::tick()
+{
+    oldTime = newTime;
+    newTime = high_resolution_clock::now();
+}
+
+// Returns time elapsed in seconds as a float
+float StopWatch::getTimeElapsed() const
+{
+    return (duration_cast<duration<float>>(newTime - oldTime)).count();
+}
+
+#endif // STOP_H
